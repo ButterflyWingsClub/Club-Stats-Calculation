d01a06ce221ee1163a09094b39218a6fef989722
ButterflyWingsClub <lpclubbw@gmail.com>
2026-02-01 06:05:08 +0530

    Update stats.js

--- stats.js
+++ stats.js
@@ -11,8 +11,8 @@ module.exports = async function runStatsExtractor(page) {
 
   // ✅ ENTER ALL CLUB URLs HERE
   const clubUrls = [
-    "https://v3.g.ladypopular.com/guilds.php?id=24", 
-    //"https://v3.g.ladypopular.com/guilds.php?id=636", 
+    "https://v3.g.ladypopular.com/guilds.php?id=1891", 
+    "https://v3.g.ladypopular.com/guilds.php?id=3007", 
     //"https://v3.g.ladypopular.com/guilds.php?id=4733", 
     //"https://v3.g.ladypopular.com/guilds.php?id=90", 
     // "https://v3.g.ladypopular.com/guilds.php?id=24",
